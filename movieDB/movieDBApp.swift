9767469a3582519eeae1919b950c4813faeccd37
Aline Escobar <aline.escobar@outlook.com>
2021-03-25 14:59:03 -0300

    Initial Commit

---
+++ movieDB/movieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieDBApp.swift
+//  movieDB
+//
+//  Created by Aline Osana Escobar on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct movieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
