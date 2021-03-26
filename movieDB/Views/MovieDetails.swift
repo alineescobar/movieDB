5adf3f2efa04ce0ae003a073adb9678d5d008799
Aline Escobar <aline.escobar@outlook.com>
2021-03-26 17:09:20 -0300

    creating the view of the movie details - layout

--- movieDB/Views/MovieDetails.swift
+++ movieDB/Views/MovieDetails.swift
@@ -8,13 +8,69 @@
 import SwiftUI
 
 struct MovieDetails: View {
+    var movie: Movie
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                HStack{
+                    Image(movie.title)
+                        .resizable()
+                        .frame(width: 100, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .clipShape(RoundedRectangle(cornerRadius: 15.0))
+                        .padding(5)
+                    
+                }
+                VStack{
+                    HStack {
+                        Text(movie.title)
+                            .font(.title2)
+                            .padding(5)
+                            .frame(alignment: .leading)
+                        Spacer()
+                    }
+                    HStack {
+                        Text(movie.genre)
+                            .font(.caption)
+                            .frame(alignment: .leading)
+                        Spacer()
+                    }
+                    HStack{
+                        Text("⭐️")
+                        Text(String(movie.rate))
+                            .frame(alignment: .leading)
+                        Spacer()
+                    }
+                }
+            }
+            
+            VStack{
+                HStack {
+                    Text("Overview")
+                        .frame(alignment: .leading)
+                    Spacer()
+                }
+                
+                HStack{
+                    Text("Release date: \(String(movie.year))")
+                    Text("Duration: \(movie.duration)")
+                        .frame(alignment: .leading)
+                    Spacer()
+                    
+                }
+                Text(movie.description)
+                    .font(.body)
+                    .padding(5)
+                    .frame(alignment: .leading)
+                Spacer()
+            }
+        }
+        
     }
 }
 
 struct MovieDetails_Previews: PreviewProvider {
     static var previews: some View {
-        MovieDetails()
+        MovieDetails(movie: movies[0])
     }
 }
