50f83b68c73b65b68818c525f13b53a9f8b38ca9
Aline Escobar <aline.escobar@outlook.com>
2021-03-26 16:21:10 -0300

    movie detail page

---
+++ movieDB/Views/MovieDetails.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetails.swift
+//  movieDB
+//
+//  Created by Aline Osana Escobar on 26/03/21.
+//
+
+import SwiftUI
+
+struct MovieDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetails()
+    }
+}
